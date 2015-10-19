0d221fbb7df0902219435409c53fd28deda56695
Hans Petter Langtangen <hpl@simula.no>
2015-10-19 10:57:23 -0700

    updates

--- doc/.src/book/make_html.sh
+++ doc/.src/book/make_html.sh
@@ -47,7 +47,7 @@ system doconce sphinx_dir theme=$theme dirname=sphinx-${theme} $name
 system python automake_sphinx.py
 
 # Publish
-repo=~/vc/fdm-book/
+repo=../../..
 dest=${repo}/doc/pub/book
 if [ ! -d $dest ]; then mkdir $dest; fi
 if [ ! -d $dest/html ]; then mkdir $dest/html; fi
